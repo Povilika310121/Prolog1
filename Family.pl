90e7998035c6617370a4d4fbcb60be21c2428476
Vika <73997912+Povilika310121@users.noreply.github.com>
2021-02-12 11:40:05 +0300

    Задание 4
    Предикаты father(X, Y), father(X),

--- Family.pl
+++ Family.pl
@@ -72,3 +72,6 @@ children(X):-parent(X,Y),write(Y),nl,fail.
 
 mother(X,Y):-parent(X,Y),woman(X).
 mother(X):-mother(Y,X),write(Y).
+
+father(X,Y):-parent(X,Y),man(X).
+father(X):-father(Y,X),write(Y).
