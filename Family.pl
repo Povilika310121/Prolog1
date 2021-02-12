b86d443c020eb8ddd9bb40f0dd9476db37506bd7
Vika <73997912+Povilika310121@users.noreply.github.com>
2021-02-12 12:38:38 +0300

    Задание 11
    Предикат grand_ma_and_son(X,Y),

--- Family.pl
+++ Family.pl
@@ -88,3 +88,6 @@ grand_mas(Y):-grand_ma(X,Y),write(X),nl,fail.
 
 grand_pa_and_son(X,Y):-grand_pa(X,Y),man(Y).
 grand_pa_and_son(X,Y):-grand_pa(Y,X),man(X).
+
+grand_ma_and_son(X,Y):-grand_ma(X,Y),man(Y).
+grand_ma_and_son(X,Y):-grand_ma(Y,X),man(X).
