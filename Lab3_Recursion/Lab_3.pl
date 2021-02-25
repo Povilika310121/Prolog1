3aab2fe52c45db925d54774a1406299f3cb921cd
Vika <73997912+Povilika310121@users.noreply.github.com>
2021-02-25 21:05:40 +0300

    Задание 5

--- Lab3_Recursion/Lab_3.pl
+++ Lab3_Recursion/Lab_3.pl
@@ -16,3 +16,9 @@ fact(N,X):-N1 is N-1,fact(N1,X1),X is X1*N.
 fact1(N,X):-fact1(0,1,N,X).
 fact1(N,X,N,X):-!.
 fact1(I,F,N,X):-I1 is I+1,F1 is F*I1,fact1(I1,F1,N,X).
+
+% Задание 5 Реализовать предикат fib(N,X), где X – число Фибоначчи с
+% номером N, причем 1 и 2 элемент равны 1 с помощью рекурсии вверх.
+fib(1,1):-!.
+fib(2,1):-!.
+fib(N,X):-N1 is N-1,N2 is N-2, fib(N1,X1),fib(N2,X2), X is X1+X2.
