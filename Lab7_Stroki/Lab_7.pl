dde6838245c0569074cf19ff40fe513f31ce4c8d
Vika <73997912+Povilika310121@users.noreply.github.com>
2021-03-20 14:03:15 +0300

    Update

--- Lab7_Stroki/Lab_7.pl
+++ Lab7_Stroki/Lab_7.pl
@@ -1,7 +1,20 @@
-%Задание 1 Дана строка. Вывести ее три раза через запятую и показать количество символов в ней.
+write_str([]):-!.
+write_str([H|Tail]):-put(H),write_str(Tail).
+
+write_list_str([]):-!.
+write_list_str([H|Tail]):-write_str(H),write(" "),write_list_str(Tail).
+
+read_str(A,N):-get0(X),r_str(X,A,[],N,0).
+r_str(10,A,A,N,N):-!.
+r_str(X,A,B,N,K):-K1 is K+1,append(B,[X],B1),get0(X1),r_str(X1,A,B1,N,K1).
+
+in_list([El|_],El).
+in_list([_|T],El):-in_list(T,El).
+
+%Задание 1
 pr(X):-read_str(List,X) , write_str(List), Z = ",", write(Z),  write_str(List), write(Z),  write_str(List).
 
-%Задание2 Дана строка. Найти количество слов.
+%Задание 2
 read_str1([H|T],N):-r_str1(H,T,N,0).
 r_str1(_,[],I,N):-I is N+1,!.
 r_str1(32,[H|T],I,N):-I1 is N+1, r_str1(H,T , I, I1),!.
