5a942efacd6e4efe7b13fcaf3218e2fbcd376d4e
Vika <73997912+Povilika310121@users.noreply.github.com>
2021-03-22 10:54:42 +0300

    Задание 5

--- Lab7_Stroki/lab_7_1.pl
+++ Lab7_Stroki/lab_7_1.pl
@@ -15,7 +15,8 @@ in_list([_|T],El):-in_list(T,El).
 append1([],List,List):-!.
 append1([H1|T1],List,[H1|T3]):-append(T1,List,T3).
 
-pr(X):-read_str(List,X) , write_str(List), Z = ",", write(Z),  write_str(List), write(Z),  write_str(List).
+pr(X):-read_str(List,X) , write_str(List), Z = ",", write(Z),  write_str(List),
+    write(Z),  write_str(List).
 
 %Задание 2
 read_str1([H|T],N):-r_str1(H,T,N,0).
@@ -28,22 +29,16 @@ r_str1(_,[H|T],I,N):-r_str1(H, T, I, N).
 list_words:-read_str(A,_),append1([32],A,A1),reverse(A1,AR),list_words(AR,[],_,[]).
 list_words([],LW,LW,_):-!.
 list_words([H|T],LW,LWN,W):-(H=32 -> append([W],LW,LW1),list_words(T,LW1,LWN,[]);
-						 append1([H],W,W1),list_words(T,LW,LWN,W1)).
+append1([H],W,W1),list_words(T,LW,LWN,W1)).
 
 kol_repeat_in_list([H|T],X,K):-kol_repeat_in_list([H|T],X,0,K).
 kol_repeat_in_list([],_,Kol,Kol):-!.
-kol_repeat_in_list([H|T],X,K,Kol):-(H=X -> K1 is K+1,kol_repeat_in_list(T,X,K1,Kol);
-									kol_repeat_in_list(T,X,K,Kol)).
+kol_repeat_in_list([H|T],X,K,Kol):-(H=X -> K1 is K+1,kol_repeat_in_list(T,X,K1,Kol);	kol_repeat_in_list(T,X,K,Kol)).
 
 often_word_in_list:-read_str(A,_),append1([32],A,A1),reverse(A1,AR),list_words(AR,[],LW,[]),often_word_in_list(LW,_,Word,0,_),write_str(Word),!.
 often_word_in_list([],Word,Word,Kol,Kol):-!.
-often_word_in_list([H|T],W,Word,K,Kol):-kol_repeat_in_list([H|T],H,K1),(K1>K -> Kol1 = K1,W1=H,
-								  often_word_in_list(T,W1,Word,K1,Kol1);often_word_in_list(T,W,Word,K,Kol)).
+often_word_in_list([H|T],W,Word,K,Kol):-kol_repeat_in_list([H|T],H,K1),(K1>K -> Kol1 = K1,W1=H,often_word_in_list(T,W1,Word,K1,Kol1);often_word_in_list(T,W,Word,K,Kol)).
 
-often_word_in_list:-read_str(A,_),append1([32],A,A1),reverse(A1,AR),list_words(AR,[],LW,[]),often_word_in_list(LW,_,Word,0,_),write_str(Word),!.
-often_word_in_list([],Word,Word,Kol,Kol):-!.
-often_word_in_list([H|T],W,Word,K,Kol):-kol_repeat_in_list([H|T],H,K1),(K1>K -> Kol1 = K1,W1=H,
-								  often_word_in_list(T,W1,Word,K1,Kol1);often_word_in_list(T,W,Word,K,Kol)).
 %Задание 4
 pr4:-read_str(A,Length),(Length>5 -> pr4(A),reverse(A,AR),pr42(AR);
 	   concl(A,Length)).
@@ -51,4 +46,10 @@ pr42([H1|[H2|[H3|_]]]):-put(H3),put(H2),put(H1),!.
 pr4([H1|[H2|[H3|_]]]):-put(H1),put(H2),put(H3),!.
 pr4([_|_],0):-!.
 pr4([H|T],Length):-put(H),L1 is Length-1,pr4([H|T],L1).
+
+%Задание 5
+index_end:-read_str(Sr,_),reverse(Sr,[H|_]),EndS = H,list_el(Sr,EndS).
+list_el([H|T],El):-list_el([H|T],El,0).
+list_el([],_,_):-!.
+list_el([H|T],El,Index):-Index1 is Index+1,(H = El-> write(Index1),write(" "),list_el(T,El,Index1);list_el(T,El,Index1)).
 
