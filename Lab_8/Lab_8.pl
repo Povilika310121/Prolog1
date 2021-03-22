c0e6e8aecff9ed2e2d47a0b3a9f7c5d6880e62f9
Vika <73997912+Povilika310121@users.noreply.github.com>
2021-03-22 21:47:45 +0300

    Задание 1

---
+++ Lab_8/Lab_8.pl
@@ -0,0 +1,17 @@
+read_str(A,N,Flag):-get0(X),r_str(X,A,[],N,0,Flag).
+r_str(-1,A,A,N,N,1):-!.
+r_str(10,A,A,N,N,0):-!.
+r_str(X,A,B,N,K,Flag):-K1 is K+1,append(B,[X],B1),get0(X1),r_str(X1,A,B1,N,K1,Flag).
+
+read_list_str(List, LengthList):-read_str(A,N,Flag),read_list_str([A],List,[N],LengthList,Flag).
+read_list_str(List,List,LengthList, LengthList,1):-!.
+read_list_str(Cur_list,List,CurLengthList,LengthList,0):-
+	read_str(A,N,Flag),append(Cur_list,[A],C_l),append(CurLengthList, [N], NewLengthList),read_list_str(C_l,List,NewLengthList,LengthList,Flag).
+
+max(List, MaxEl):- max(List, 0, MaxEl).
+max([],CurMax, CurMax):- !.
+max([H|T], CurMax, X):- H > CurMax, NewMax is H, max(T,NewMax,X), !.
+max([_|T], CurMax, X):- max(T, CurMax, X).
+
+%__1__
+pr5_1:- see('c:/Users/��������/Documents/GitHub/Prolog1/Lab_8/labb.txt'),read_list_str(List, Lenght), seen,max(Lenght, Max), write(Max).
