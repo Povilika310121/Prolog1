58935f8936e08d5edd9b28ebc75c6a3c8409de00
Vika <73997912+Povilika310121@users.noreply.github.com>
2021-03-24 20:36:35 +0300

    Задание 2

--- Lab_8/Lab_8.pl
+++ Lab_8/Lab_8.pl
@@ -13,5 +13,23 @@ max([],CurMax, CurMax):- !.
 max([H|T], CurMax, X):- H > CurMax, NewMax is H, max(T,NewMax,X), !.
 max([_|T], CurMax, X):- max(T, CurMax, X).
 
+append1([],List2,List2).
+append1([H1|T1],List2,[H1|T3]):-append1(T1,List2,T3).
+
+write_str([]):-!.
+write_str([H|Tail]):-put(H),write_str(Tail).
+
+write_list_str([]):-!.
+write_list_str([H|T]):-write_str(H),nl,write_list_str(T).
+
+in_list([El|_],El).
+in_list([_|T],El):-in_list(T,El).
+
 %__1__
-pr5_1:- see('c:/Users/пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ/Documents/GitHub/Prolog1/Lab_8/labb.txt'),read_list_str(List, Lenght), seen,max(Lenght, Max), write(Max).
+pr8_1:- see('c:/Users/Виктория/Documents/GitHub/Prolog1/Lab_8/labb.txt'),read_list_str(_, Lenght), seen,max(Lenght, Max), write(Max).
+
+pr8_2:- see('c:/Users/Виктория/Documents/GitHub/Prolog1/Lab_8/labb.txt'),read_list_str(List,_), seen, space(List, 0, Kolvo),write(Kolvo).
+space([],K,K):-!.
+space([H|T], K, Kolvo):-not(in_list(H,32)),K1 is K+1, space(T, K1, Kolvo),!.
+space([_|T], K, Kolvo):-space(T, K, Kolvo),!.
+
