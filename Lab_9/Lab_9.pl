7b4eacf48525d6f7173294cf46f665e209e00d31
Vika <73997912+Povilika310121@users.noreply.github.com>
2021-03-29 10:41:38 +0300

    1.1

---
+++ Lab_9/Lab_9.pl
@@ -0,0 +1,24 @@
+write_str([]):-!.
+write_str([H|Tail]):-put(H),write_str(Tail).
+in_list([El|_],El).
+in_list([_|T],El):-in_list(T,El).
+
+read_str(A,N):-get0(X),r_str(X,A,[],N,0).
+r_str(10,A,A,N,N):-!.
+r_str(X,A,B,N,K):-K1 is K+1,append(B,[X],B1),get0(X1),r_str(X1,A,B1,N,K1).
+
+read_list_str(List):-read_str(A,N,Flag),read_list_str([A],List,Flag).
+read_list_str(List,List,1):-!.
+read_list_str(Cur_list,List,0):-
+	read_str(A,N,Flag),append(Cur_list,[A],C_l),read_list_str(C_l,List,Flag).
+
+%1.1
+build_all_perm:-
+		read_str(A,_),b_a_p(A,[]).
+
+in_list_exlude([El|T],El,T).
+in_list_exlude([H|T],El,[H|Tail]):-in_list_exlude(T,El,Tail).
+
+b_a_p([],Perm1):-write_str(Perm1),nl,!,fail.
+b_a_p(A,Perm):-in_list_exlude(A,El,A1),b_a_p(A1,[El|Perm]).
+
